37464a487baa49bddb1d758e869a28d274dab247
Yeiko Giron <yeikogianmarco@gmail.com>
2025-08-20 22:32:01 -0500

    Funcion actualizar lista

--- app.js
+++ app.js
@@ -14,5 +14,17 @@ function agregarAmigo(){
 
     input.value = "";
 
+    actualizarLista();
+
+}
+
+function actualizarLista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
 }
 
